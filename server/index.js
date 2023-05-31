ebc707e2f8ff0260e7ac59caf951c9bc2facf41c
egusev <shchelkalinevgeniy@gmail.com>
2023-05-31 18:09:30 +0300

    feat: mongodb connect

--- server/index.js
+++ server/index.js
@@ -1,15 +1,44 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.DB_URI)
+  .then(() => {
+    console.log('DATABASE OK');
+  })
+  .catch((err) => {
+    console.log('DATABASE ERROR', err);
+  })
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Hello, friend!');
+  res.send('Hello, friend!!!');
+});
+
+app.post('/auth/sign_in', (req, res) => {
+  const { email, password } = req.body;
+
+  const token = jwt.sign({
+	  email,
+	  password,
+  }, 'secret', { expiresIn: '2m' });
+
+  res.json({
+    success: true,
+    token,
+  })
 });
 
 app.listen(4444, (err) => {
   if (err) {
     return console.log(err)
-  } else {
-    return console.log('OK');
   }
+
+  console.log('SERVER OK');
 });
